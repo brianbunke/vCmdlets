a7e790437e82d2ea4e6273369b61dcf13794c9d4
Brian Bunke <brianbunke+github@gmail.com>
2018-11-29 20:22:42 -0800

    Update unit tests for vcsim CI

--- Get-DatastoreProvisioned.Tests.ps1
+++ Get-DatastoreProvisioned.Tests.ps1
@@ -1,91 +1,46 @@
-﻿# To run: "Invoke-Pester <path>\Get-DatastoreProvisioned.Tests.ps1"
+﻿Describe 'Get-DatastoreProvisioned' -Tag unit {
+    ### ARRANGE
+    
+    # Dot source the function
+    . $PSScriptRoot\Get-DatastoreProvisioned.ps1
 
-<#
-Script name: Get-DatastoreProvisioned.Tests.ps1
-Created on: 2016/07/27
-Author: Brian Bunke, @brianbunke
-Description: Help validate that any changes to Get-DatastoreProvisioned.ps1 do not break existing functionality
-Dependencies: Pester
-#>
+    ### ACT
 
-<# ### This block is for use in the PowerCLI Example Scripts repository
-    # Tests file currently stored separately from actual script
-    # Find where this file is running from, replace parent folder 'Pester' with 'Scripts'
-    $Path = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace('Pester','Scripts')
-    # Remove the '.Tests.' from the file name
-    $File = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
-    # With changes made to the path, dot-source the function for testing
-    . "$Path\$File"
-### Instead, using this to load the cmdlet: #>
-. .\Get-DatastoreProvisioned.ps1
+    # Connect to the vcsim Docker container running locally
+    Connect-VIServer -Server localhost -Port 443 -User u -Password p
 
-Describe 'Get-DatastoreProvisioned' {
-    # Need to create a few example objects to proxy Get-Datastore pipeline input
-    $1 = [PSCustomObject]@{
-        Name          = 'iSCSI-spin'
-        CapacityGB    = 38.40
-        FreeSpaceGB   = 15.55
-        ExtensionData = @{
-            Summary = @{
-                Capacity    = 41234567890
-                FreeSpace   = 16696685366
-                Uncommitted = 12345678999
-    }}}
-    $2 = [PSCustomObject]@{
-        Name          = 'iSCSI-ssd'
-        CapacityGB    = 51.74
-        FreeSpaceGB   = 10.35
-        ExtensionData = @{
-            Summary = @{
-                Capacity    = 55555555555
-                FreeSpace   = 11111111111
-                Uncommitted = 23456765432
-    }}}
-    $3 = [PSCustomObject]@{
-        Name          = 'FC-ssd'
-        CapacityGB    = 10.35
-        FreeSpaceGB   = 4.14
-        ExtensionData = @{
-            Summary = @{
-                Capacity    = 11111111111
-                FreeSpace   = 4444444444
-                Uncommitted = 2222222222
-    }}}
+    # Capture the datastore for further interaction
+    $ds0 = Get-Datastore -Name LocalDS_0
+    $ds1 = Get-Datastore -Name LocalDS_1
 
-    It "Doesn't change existing functionality" {
-        $StillWorks = $1,$2,$3 | Get-DatastoreProvisioned
-        # Should see all three proxied objects
-        $StillWorks | Should Not BeNullOrEmpty
-        ($StillWorks | Measure-Object).Count | Should Be 3
-        # Should still have six properties with the same names
-        ($StillWorks | Get-Member -MemberType NoteProperty).Count | Should Be 6
-        'Name','FreeSpaceGB','CapacityGB','ProvisionedGB','UsedPct','ProvisionedPct' | ForEach-Object {
-            ($StillWorks | Get-Member -MemberType NoteProperty).Name -contains $_ | Should Be $true
-        }
+    $Pipe1 = $ds0 | Get-DatastoreProvisioned
+    $Pipe2 = $ds0, $ds1 | Get-DatastoreProvisioned
+
+    ### ASSERT
+    
+    It 'Receives expected ds0 values from vcsim' {
+        # vcsim container defaults
+        $ds0.CapacityMB  | Should -Be 58
+        $ds0.FreeSpaceMB | Should -Be 53
+        $ds0.ExtensionData.Summary.Capacity    | Should -Be 61664044
+        $ds0.ExtensionData.Summary.FreeSpace   | Should -Be 56188984
+        $ds0.ExtensionData.Summary.Uncommitted | Should -BeNullOrEmpty
     }
 
-    It 'Still calculates correctly' {
-        $calc = $1 | Get-DatastoreProvisioned
-        $calc | Should Not BeNullOrEmpty
-        $calc.ProvisionedGB | Should Be 34.35
-        $calc.UsedPct | Should Be 59.51
-        $calc.ProvisionedPct | Should Be 89.45
+    It 'Receives expected ds1 values from vcsim' {
+        # Second datastore will have the same values. Just check one
+        $ds1.CapacityMB | Should -Be 58
     }
 
-    # Get-Datastore | Get-DatastoreProvisioned | Format-Table -AutoSize
-    It 'Follows Help Example 1' {
-        $Help1 = $1,$2,$3 | Get-DatastoreProvisioned
-        $Help1 | Should Not BeNullOrEmpty
-        ($Help1 | Measure-Object).Count | Should Be 3
-        # not testing Format-Table
+    It 'Correctly calculates values' {
+        $Pipe1.FreeSpaceGB    | Should -Be .05
+        $Pipe1.CapacityGB     | Should -Be .06
+        $Pipe1.ProvisionedGB  | Should -Be .01
+        $Pipe1.UsedPct        | Should -Be 8.62
+        $Pipe1.ProvisionedPct | Should -Be 9
     }
 
-    # Get-Datastore -Name '*ssd' | Get-DatastoreProvisioned | Where-Object ProvisionedPct -ge 100
-    It 'Follows Help Example 2' {
-        $Help2 = $1,$2,$3 | Where Name -like '*ssd' | Get-DatastoreProvisioned | Where ProvisionedPct -ge 100
-        $Help2 | Should Not BeNullOrEmpty
-        ($Help2 | Measure-Object).Count | Should Be 1
-        $Help2.Name | Should BeExactly 'iSCSI-ssd'
-        $Help2.ProvisionedPct | Should BeGreaterThan 100
+    It 'Processes multiple objects via the pipeline' {
+        $Pipe2.Count | Should -Be 2
     }
 }
