2014741e5aa05295ff6afa450b0da90e03c245fb
Brian Bunke <brianbunke+github@gmail.com>
2019-09-06 22:06:32 -0700

    Update test

--- Tests/Get-DatastoreProvisioned.Tests.ps1
+++ Tests/Get-DatastoreProvisioned.Tests.ps1
@@ -1,52 +1,32 @@
-﻿Describe 'Connectivity' -Tag unit {
-    It 'Sees the Docker container' {
-        Test-Connection localhost -TCPPort 443 | Should -BeTrue
-    }
-}
-
-Describe 'Get-DatastoreProvisioned' -Tag unit {
+﻿Describe 'Get-DatastoreProvisioned' -Tag unit {
     ### ARRANGE
     
     # Dot source the function
-    . $PSScriptRoot\Get-DatastoreProvisioned.ps1
+    . $PSScriptRoot\..\Get-DatastoreProvisioned.ps1
 
     ### ACT
 
     # Capture the datastore for further interaction
     $ds0 = Get-Datastore -Name LocalDS_0
-    $ds1 = Get-Datastore -Name LocalDS_1
 
     # Run the command twice, storing the results for assertions
     $Pipe1 = $ds0 | Get-DatastoreProvisioned
-    $Pipe2 = $ds0, $ds1 | Get-DatastoreProvisioned
+    $Pipe2 = $ds0, $ds0 | Get-DatastoreProvisioned
 
     ### ASSERT
     
     It 'Receives expected ds0 values from vcsim' {
         # vcsim container defaults
-        $ds0.CapacityMB  | Should -Be 124
-        $ds0.FreeSpaceMB | Should -Be 92
-
-        # Exempting these tests because the numbers vary (e.g. 96935068 vs. 96935152)
-        # Hoping the calculations below are good enough
-        <#
-        $ds0.ExtensionData.Summary.Capacity    | Should -Be 130046416
-        $ds0.ExtensionData.Summary.FreeSpace   | Should -Be 96935068
-        $ds0.ExtensionData.Summary.Uncommitted | Should -BeNullOrEmpty
-        #>
-    }
-
-    It 'Receives expected ds1 values from vcsim' {
-        $ds1.CapacityMB  | Should -Be 124
-        $ds1.FreeSpaceMB | Should -Be 92
+        $ds0.CapacityMB  | Should -Be 58
+        $ds0.FreeSpaceMB | Should -Be 53
     }
 
     It 'Correctly calculates values' {
-        $Pipe1.FreeSpaceGB    | Should -Be .09
-        $Pipe1.CapacityGB     | Should -Be .12
-        $Pipe1.ProvisionedGB  | Should -Be .03
-        $Pipe1.UsedPct        | Should -Be 25.81
-        $Pipe1.ProvisionedPct | Should -Be 25.47
+        $Pipe1.FreeSpaceGB    | Should -Be .05
+        $Pipe1.CapacityGB     | Should -Be .06
+        $Pipe1.ProvisionedGB  | Should -Be 0
+        $Pipe1.UsedPct        | Should -Be 8.62
+        $Pipe1.ProvisionedPct | Should -Be 8.37
     }
 
     It 'Processes multiple objects via the pipeline' {
